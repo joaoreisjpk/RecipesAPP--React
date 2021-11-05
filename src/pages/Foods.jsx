2a188f8c154b5aa861554179a86b9cfa3e0ce054
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-05 18:23:58 -0300

    Requisito 9

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import HeaderWithSearchIcon from '../components/HeaderWithSearchIcon';
 
 function Foods() {
   return (
-    <div />
+    <HeaderWithSearchIcon title="Comidas" />
   );
 }
 
