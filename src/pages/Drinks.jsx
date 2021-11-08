8966633a8e458ab104de71fc41e7a54e68a771e2
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-08 20:50:34 -0300

    Requisito 21

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import HeaderWithSearchIcon from '../components/HeaderWithSearchIcon';
 import MyContext from '../context/MyContext';
 import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 function Drinks() {
   const { respostaDrink } = useContext(MyContext);
@@ -25,6 +26,7 @@ function Drinks() {
           name={ item.strDrink }
         />
       )).slice(0, TREZE)}
+      <Footer />
     </div>
   );
 }
