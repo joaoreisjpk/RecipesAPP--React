de833f7e0095cdf055c6994a8eca9ea55052d705
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-08 20:46:35 -0300

    Requisito 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,14 @@ import exploreIcon from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn">
+    <footer style={ { position: 'fixed', bottom: '0px' } } data-testid="footer">
+      <button type="button" data-testid="drinks-bottom-btn" src={ drinkIcon }>
         <img src={ drinkIcon } alt="Drink Icon" />
-
       </button>
-      <button type="button" data-testid="explore-bottom-btn">
+      <button type="button" data-testid="explore-bottom-btn" src={ exploreIcon }>
         <img src={ exploreIcon } alt="Explore Icon" />
-
       </button>
-      <button type="button" data-testid="food-bottom-btn">
+      <button type="button" data-testid="food-bottom-btn" src={ mealIcon }>
         <img src={ mealIcon } alt="Meal Icon" />
       </button>
     </footer>
