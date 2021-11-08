8f76651c43e7b7d67d63926a99c8ad65312b2f27
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-08 20:52:47 -0300

    Requisitos 22 ao 24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
@@ -6,15 +7,21 @@ import exploreIcon from '../images/exploreIcon.svg';
 function Footer() {
   return (
     <footer style={ { position: 'fixed', bottom: '0px' } } data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn" src={ drinkIcon }>
-        <img src={ drinkIcon } alt="Drink Icon" />
-      </button>
-      <button type="button" data-testid="explore-bottom-btn" src={ exploreIcon }>
-        <img src={ exploreIcon } alt="Explore Icon" />
-      </button>
-      <button type="button" data-testid="food-bottom-btn" src={ mealIcon }>
-        <img src={ mealIcon } alt="Meal Icon" />
-      </button>
+      <Link to="/bebidas">
+        <button type="button" data-testid="drinks-bottom-btn" src={ drinkIcon }>
+          <img src={ drinkIcon } alt="Drink Icon" />
+        </button>
+      </Link>
+      <Link to="/explorar">
+        <button type="button" data-testid="explore-bottom-btn" src={ exploreIcon }>
+          <img src={ exploreIcon } alt="Explore Icon" />
+        </button>
+      </Link>
+      <Link to="/comidas">
+        <button type="button" data-testid="food-bottom-btn" src={ mealIcon }>
+          <img src={ mealIcon } alt="Meal Icon" />
+        </button>
+      </Link>
     </footer>
   );
 }
