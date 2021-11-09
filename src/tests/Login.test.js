33754b3a922b7388baf0d42bd525115e56fe947d
João Reis <joaoreisjpk@gmail.com>
2021-11-09 13:58:52 -0300

    ajeitando os testes do loginPage e iniciando os mocks de futuros testes

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -7,73 +7,73 @@ import renderWithRouter from './helper/renderWithRouter';
 const EMAIL = 'trybe@tryber.com';
 const PASSWORD = '12345678';
 
+const getEmail = () => screen.getByRole('textbox', {
+  name: /e-mail/i,
+});
+const getPassword = () => screen.getByLabelText(/senha/i);
+const getButton = () => screen.getByRole('button', {
+  name: /entrar/i,
+});
+
 describe('Testa se a tela de Login funciona devidamente', () => {
-  beforeEach(() => renderWithRouter(<App />));
-  renderWithRouter(<App />);
-
-  const getEmail = screen.getByRole('textbox', {
-    name: /e-mail/i,
+  test('1 - Testa se os componentes aparecem na tela', () => {
+    renderWithRouter(<App />);
+    expect(getEmail()).toBeInTheDocument();
+    expect(getPassword()).toBeInTheDocument();
+    expect(getButton()).toBeInTheDocument();
   });
 
-  const getPassword = screen.getByLabelText(/senha/i);
-
-  const getButton = screen.getByRole('button', {
-    name: /entrar/i,
+  test('2 - É possível digitar no campo e-mail', () => {
+    renderWithRouter(<App />);
+    userEvent.type(getEmail(), EMAIL);
   });
 
-  test('2 - Testa se os componentes aparecem na tela', () => {
-    expect(getEmail).toBeInTheDocument();
-    expect(getPassword).toBeInTheDocument();
-    expect(getButton).toBeInTheDocument();
+  test('3 - É possível digitar no campo senha', () => {
+    renderWithRouter(<App />);
+    userEvent.type(getPassword(), PASSWORD);
   });
 
-  test('3 - É possível digitar no campo e-mail', () => {
-    userEvent.type(getEmail, EMAIL);
-  });
-
-  test('4 - É possível digitar no campo senha', () => {
-    userEvent.type(getPassword, PASSWORD);
-  });
-
-  test(`5 - Se o botão só é habilitado após um e-mail válido e uma
+  test(`4 - Se o botão só é habilitado após um e-mail válido e uma
   senha maior que 6 caracteres`, async () => {
-    expect(getButton).toBeDisabled();
-    userEvent.type(getEmail, '123');
-    userEvent.type(getPassword, '123');
-    expect(getButton).toBeDisabled();
+    renderWithRouter(<App />);
+    expect(getButton()).toBeDisabled();
+    userEvent.type(getEmail(), '123');
+    userEvent.type(getPassword(), '123');
+    expect(getButton()).toBeDisabled();
 
-    userEvent.type(getPassword, PASSWORD);
-    expect(getButton).toBeDisabled();
+    userEvent.type(getPassword(), PASSWORD);
+    expect(getButton()).toBeDisabled();
 
-    userEvent.type(getEmail, EMAIL);
-    expect(getPassword.value).toBe(PASSWORD);
-    expect(getEmail.value).toBe(EMAIL);
-    expect(getButton).not.toBeDisabled();
+    userEvent.type(getEmail(), EMAIL);
+    expect(getPassword().value).toBe(PASSWORD);
+    expect(getEmail().value).toBe(EMAIL);
+    expect(getButton()).not.toBeDisabled();
   });
 
-  test(`6 - Se os dois tokens "mealsToken" e "cocktailsToken" são guardados no 
+  test(`5 - Se os dois tokens "mealsToken" e "cocktailsToken" são guardados no 
     localStorage após a submissão`, () => {
-    userEvent.type(getEmail, EMAIL);
-    userEvent.type(getPassword, PASSWORD);
-    userEvent.click(getButton);
+    renderWithRouter(<App />);
+    userEvent.type(getEmail(), EMAIL);
+    userEvent.type(getPassword(), PASSWORD);
+    userEvent.click(getButton());
     expect(localStorage.getItem('mealsToken')).toBe('1');
     expect(localStorage.getItem('cocktailsToken')).toBe('1');
   });
 
-  test(`7 - Se o email da pessoa usuária foi salvo no Local Storage 
+  test(`6 - Se o email da pessoa usuária foi salvo no Local Storage 
   na chave user após submissão`, () => {
-    userEvent.type(getEmail, EMAIL);
-    userEvent.type(getPassword, PASSWORD);
-    userEvent.click(getButton);
-    expect(localStorage.getItem('user')).toBe(`{ email: ${EMAIL}}`);
+    renderWithRouter(<App />);
+    userEvent.type(getEmail(), EMAIL);
+    userEvent.type(getPassword(), PASSWORD);
+    userEvent.click(getButton());
+    expect(localStorage.getItem('user')).toBe(`{"email":"${EMAIL}"}`);
   });
 
-  test('8 - Ao clicar no botao de Entrar muda para a rota /comidas', () => {
+  test('7 - Ao clicar no botao de Entrar muda para a rota /comidas', () => {
     const { history } = renderWithRouter(<App />);
-    userEvent.type(getEmail, 'trybe@tryber.com');
-    fireEvent.change(getPassword, { target: { value: '12345678' } });
-    // userEvent.type(getPassword, '12345678');
-    userEvent.click(getButton);
+    userEvent.type(getEmail(), EMAIL);
+    fireEvent.change(getPassword(), { target: { value: PASSWORD } });
+    userEvent.click(getButton());
     expect(history.location.pathname).toEqual('/comidas');
   });
 });
