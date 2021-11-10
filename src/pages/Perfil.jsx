222ecea63f3453cb036c7a7ad18ef44258785ccd
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-10 00:46:38 -0300

    Requisitos 82 ao 87

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Button from '../components/Button';
 
 function Perfil() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <>
       <Header title="Perfil" />
+      <h4 data-testid="profile-email">{email}</h4>
+      <Link to="/receitas-feitas">
+        <Button dataID="profile-done-btn" text="Receitas Feitas" />
+      </Link>
+      <Link to="/receitas-favoritas">
+        <Button dataID="profile-favorite-btn" text="Receitas Favoritas" />
+      </Link>
+      <Link to="/">
+        <Button
+          dataID="profile-logout-btn"
+          text="Sair"
+          onClick={ () => localStorage.clear() }
+        />
+      </Link>
       <Footer />
     </>
   );
