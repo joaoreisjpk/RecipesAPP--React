64fcb916be6d64d8d9983b0be2e59f4bf3594816
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-11-11 13:54:02 -0300

    Criando componente para renderizar os ingredientes

---
+++ src/components/IngredientsInProgress.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+function IngredientsInProgress({ index, ingrediente, measures }) {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div data-testid={ `${index}-ingredient-step` }>
+      <label className="completed" htmlFor={ ingrediente }>
+        <input
+          data-testid={ `${index}-ingredient-step` }
+          type="checkbox"
+          id={ ingrediente }
+          checked={ checked }
+          onClick={ () => setChecked(!checked) }
+        />
+        <span>{ `Ingrediente: ${ingrediente} - Medida: ${measures[index]}` }</span>
+      </label>
+    </div>
+  );
+}
+
+IngredientsInProgress.propTypes = {
+  index: PropTypes.number.isRequired,
+  ingrediente: PropTypes.string.isRequired,
+  measures: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default IngredientsInProgress;
