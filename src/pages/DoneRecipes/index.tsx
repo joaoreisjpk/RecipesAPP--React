35ff7337b9004839001a1716e13248b0e243bb5b
João Reis <joaoreisjpk@gmail.com>
2021-12-20 14:50:41 -0300

    jj

--- src/pages/DoneRecipes/index.tsx
+++ src/pages/DoneRecipes/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ButtonsFavoriteAndShare from '../../components/ButtonsFavoriteAndShare';
-import CopyButton from '../../components/CopyButton';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { getDoneList } from '../../helpers';
